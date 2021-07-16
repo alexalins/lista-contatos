c424db1a9f2acf69331366c5b1f34bb56bf840a6
alexalins <alexa.glinsoo@gmail.com>
2021-07-16 20:55:55 -0300

    criando componente do header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = (props) => (
+    <View style={style.container}>
+        <Text style={style.title}>{props.title}</Text>
+    </View>
+);
+
+const style = StyleSheet.create({
+    container: {
+        marginTop: 25,
+        backgroundColor: '#6ca2f7',
+        alignItems: 'center',
+        justifyContent:'center'
+    },
+    title: {
+        fontSize: 50,
+        color: '#fff'
+    }
+});
+
+export default Header;
