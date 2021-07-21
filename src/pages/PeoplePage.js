38d554ff4d5f5129ef83bf0c8bb22ea22f57ca6f
alexalins <alexa.glinsoo@gmail.com>
2021-07-21 20:30:10 -0300

    criando loading

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import axios from 'axios';
 import PeopleList from '../components/PeopleList';
 
@@ -7,27 +7,39 @@ export default class PeoplePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        peoples: []
+      peoples: [],
+      loading: false
     };
-}
+  }
 
-componentDidMount() {
-    axios.get('https://randomuser.me/api/?nat=br&results=50')
-      .then(response => {
-        const {results} = response.data;
-        this.setState({
-          peoples: results
+  componentDidMount() {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      axios.get('https://randomuser.me/api/?nat=br&results=50')
+        .then(response => {
+          const { results } = response.data;
+          this.setState({
+            peoples: results,
+            loading: false
+          });
         });
-    });
+    }, 1000);
   };
 
   render() {
     return (
-      <View>
-        {/*<Header title="Contatos" />*/}
-        <PeopleList peoples={ this.state.peoples } onPressItem={(pageParams) => { this.props.navigation.navigate('Detalhes', {people: pageParams}) }}/>
+      <View style={style.container}>
+        { this.state.loading ? <ActivityIndicator size="large" color="#6ca2f7" /> 
+          :  <PeopleList peoples={this.state.peoples} onPressItem={(pageParams) => { this.props.navigation.navigate('Detalhes', { people: pageParams }) }} />
+        }
       </View>
     );
   }
 }
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+});
